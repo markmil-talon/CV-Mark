12fca0608b3ed8caf31a965bd4dc30cadebc0d78
Mark Milrani Talon <markmil.talon@yahoo.com.ph>
2025-06-12 22:42:46 +0800

    Fix projects section alignment

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -5,11 +5,13 @@ function ProjectCard({ src, link, h3, p }) {
     <a
       href={link}
       target='_blank'>
-      <img
-        className='hover'
-        src={src}
-        alt={`${h3} logo`}
-      />
+      <div className='imgContainer'>
+        <img
+          className='hover'
+          src={src}
+          alt={`${h3} logo`}
+        />
+      </div>
       <h3>{h3}</h3>
       <p>{p}</p>
     </a>
